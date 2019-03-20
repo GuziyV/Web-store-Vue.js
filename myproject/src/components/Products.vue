473eaad877520c1509b3180e35935215079aca67
Volodymyr <35489438+GuziyV@users.noreply.github.com>
2019-03-20 17:36:48 +0200

    Base implementation of Products.vue

--- myproject/src/components/Products.vue
+++ myproject/src/components/Products.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="Products">
-      Products
+      <div class="productItem" v-for="product in products" v-bind:key="product.model"> 
+
+      </div>
   </div>
 </template>
 
 <script>
-
+import { mapState, mapActions } from 'vuex'
 export default {
   name: 'Products',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-    };
-  },
+  computed: mapState({
+    products: state => state.products.all,
+  }),
+  methods: mapActions('cart', [
+    'addProductToCart'
+  ]),
+  created () {
+    this.$store.dispatch('products/getAllProducts')
+  }
 };
 </script>
 
